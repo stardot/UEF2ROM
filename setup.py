e17e1c58cfc3a4311e99c88c1dba11ae6f39ce70
David Boddie <david@boddie.org.uk>
2018-07-11 18:20:59 +0200

    Added a setup script for building a shared C library version of the compression module.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+# Run this script to create a shared C library version of the compression
+# module used by UEF2ROM.py:
+#
+# python setup.py build_ext --inplace
+
+from distutils.core import setup
+from Cython.Build import cythonize
+
+setup(
+    ext_modules = cythonize("compressors/distance_pair.py")
+    )
