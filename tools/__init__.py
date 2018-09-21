d2f93a5ed7b7b3b2e7d7f50d4285d433f3fcf8d7
David Boddie <david@boddie.org.uk>
2018-09-21 19:17:22 +0200

    Added a function to format binary data for use in an assembler listing.

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,1 +1,12 @@
-__all__ = ["diskutils", "makedfs", "UEFfile"]
+__all__ = ["diskutils", "joystick", "makedfs", "UEFfile"]
+
+def format_data(data):
+
+    s = ""
+    i = 0
+    
+    while i < len(data):
+        s += ".byte " + ",".join(map(lambda x: "$%02x" % ord(x), data[i:i+24]))
+        i += 24
+    
+    return s
